8a0c4c320c3f93f67a7b1fa2e9c4df14ec1cb552
youssef <ybenchiir@gmail.com>
2023-06-07 14:29:09 +0000

    Update

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -3,6 +3,6 @@
 def islower(c):
     check_char = ord(c)
     if check_char >= 97 and check_char <= 122:
-       return True
+        return True
     else:
-       return False
+        return False
