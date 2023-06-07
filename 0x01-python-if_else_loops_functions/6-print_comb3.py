0ace985b6607c126212b042209afcec3ceb50fd3
youssef <ybenchiir@gmail.com>
2023-06-07 14:17:11 +0000

    Update

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 for first_digit in range(0, 8):
-    for last_digit in range(0, 9):
+    for last_digit in range(0, 10):
         if last_digit > first_digit:
             print("{}{}, ".format(first_digit, last_digit), end="")
 else:
