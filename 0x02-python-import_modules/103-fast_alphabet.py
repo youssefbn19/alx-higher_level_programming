65ea48e3a8478f282f38c9657287ba332d736494
youssef <ybenchiir@gmail.com>
2023-06-10 08:37:39 -0700

    Add upper_alphabet.py, 103-fast_alphabet.py

---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+from upper_alphabet import alphabet_uppercase
+alphabet_uppercase()
---
+++ 0x02-python-import_modules/upper_alphabet.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def alphabet_uppercase():
+    for alph in range(65, 91):
+        print("{}".format(chr(alph)), end="")
+    print()
