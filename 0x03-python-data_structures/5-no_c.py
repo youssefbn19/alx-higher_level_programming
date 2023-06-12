153a02aecf54bfa2154a3330d04cb2d4f03b6eb2
youssef <ybenchiir@gmail.com>
2023-06-12 03:40:25 -0700

    Add 5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_str = ""
+    for char in my_string:
+        if char == "c" or char == "C":
+            continue
+        new_str += char
+    return new_str
