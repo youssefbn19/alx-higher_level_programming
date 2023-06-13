57b53be5f5ec3ec761b221d899d56fa725a5b4dd
youssef <ybenchiir@gmail.com>
2023-06-13 04:46:06 -0700

    Update

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,3 +1,4 @@
+#include "lists.h"
 /**
  * is_palindrome - checks if a singly linked list is a palindrome.
  *
@@ -5,30 +6,20 @@
  *
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
-#include "lists.h"
 int is_palindrome(listint_t **head)
 {
-	listint_t *temp = NULL;
 	int len = 0, i, j;
 
 	if (head == NULL || *head == NULL)
 		return (1);
 
-	len = linked_list_len(head);
+	len = linked_list_len(*head);
 	if (len % 2 != 0)
 		return (0);
 
-	int node_n[len];
-
-	temp = *head;
-	for (i = 0; i < len; i++)
-	{
-		node_n[i] = temp->n;
-		temp = temp->next;
-	}
 	for (i = 0, j = len - 1; i < (len / 2); i++, j--)
 	{
-		if (node_n[i] != node_n[j])
+		if (node_value(*head, i) != node_value(*head, j))
 			return (0);
 	}
 	return (1);
@@ -40,16 +31,37 @@ int is_palindrome(listint_t **head)
  *
  * Return: length of a listint_t list
  */
-int linked_list_len(listint_t **h)
+int linked_list_len(listint_t *h)
 {
 	int len = 0;
 	listint_t *temp = NULL;
 
-	temp = *h;
-	while (temp != NULL)
+	temp = h;
+	while (temp)
 	{
 		temp = temp->next;
 		len++;
 	}
 	return (len);
+}
+/**
+ * node_value - gives linked list node value at specific index
+ *
+ * @h: pointer of first node of a linked list
+ * @indx: node index
+ *
+ * Return: node `n` value
+ */
+int node_value(listint_t *h, int indx)
+{
+	listint_t *temp = NULL;
+	int i = 0;
+
+	temp = h;
+	while (i < indx)
+	{
+		temp = temp->next;
+		i++;
+	}
+	return (temp->n);
 }
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -20,5 +20,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
-int linked_list_len(listint_t **h);
+int linked_list_len(listint_t *h);
+int node_value(listint_t *h, int indx);
 #endif /* LISTS_H */
