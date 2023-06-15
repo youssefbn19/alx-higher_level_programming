0cb0d6def6e10797d355d412baa1e60ffacb39d4
youssef <ybenchiir@gmail.com>
2023-06-15 05:07:09 -0700

    Update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     from functools import reduce
-
     a_dict = a_dictionary
-    if a_dict is not None:
+    if a_dict:
         best_score = reduce(
                          lambda k1, k2: k1 if a_dict[k1] > a_dict[k2] else k2,
                          list(a_dict))
         return best_score
-    else:
-        return None
