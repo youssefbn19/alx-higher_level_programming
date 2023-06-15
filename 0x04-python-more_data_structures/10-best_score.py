5bfa1de6b2a620df764477ad8c6578550266b061
youssef <ybenchiir@gmail.com>
2023-06-15 04:57:38 -0700

    Add 10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from functools import reduce
+
+
+def best_score(a_dictionary):
+    a_dict = a_dictionary
+    if a_dict:
+        best_score = reduce(
+                         lambda k1, k2: k1 if a_dict[k1] > a_dict[k2] else k2,
+                         list(a_dict))
+        return best_score
