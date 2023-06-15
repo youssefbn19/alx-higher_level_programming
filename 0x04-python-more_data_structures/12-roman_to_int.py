0a0caddd3419bfa175e8121d20fae52e1a620207
youssef <ybenchiir@gmail.com>
2023-06-15 08:31:06 -0700

    Update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,10 +2,25 @@
 def roman_to_int(roman_string):
     from functools import reduce
     chk_str = isinstance(roman_string, str)
+    res = 0
+    roman_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+                 'D': 500, 'M': 1000}
     if chk_str and roman_string:
-        roman_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
-                     'D': 500, 'M': 1000}
+
+        for char in roman_string:
+            if char not in set(list(roman_num)):
+                return res
+
         rom_to_int = list(map(lambda i: roman_num[i], roman_string))
-        res = reduce(lambda x, y: x + y if x >= y else y - x, rom_to_int)
-        return res
-    return 0
+        ln = len(rom_to_int)
+
+        for i in range(ln):
+            j = i + 1
+            if j == ln:
+                res += rom_to_int[i]
+            elif rom_to_int[i] >= rom_to_int[j]:
+                res += rom_to_int[i]
+            else:
+                res -= rom_to_int[i]
+
+    return res
