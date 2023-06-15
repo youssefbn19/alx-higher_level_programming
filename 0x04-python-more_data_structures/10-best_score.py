91e08e541f76411e67cee0ef9e42c3acfd3354d5
youssef <ybenchiir@gmail.com>
2023-06-15 04:59:51 -0700

    update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -9,3 +9,5 @@ def best_score(a_dictionary):
                          lambda k1, k2: k1 if a_dict[k1] > a_dict[k2] else k2,
                          list(a_dict))
         return best_score
+    else:
+        return None
