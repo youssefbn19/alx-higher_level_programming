6827f9bac86a1ba0d57a07f217eaa022f7ae7411
youssef <ybenchiir@gmail.com>
2023-06-28 08:40:07 -0700

    Update

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,13 +5,11 @@
 class Square:
     """ Square class that defines a square """
     def __init__(self, size=0):
-        try:
-            if size < 0:
-                raise ValueError
-            elif type(size) != int:
-                raise TypeError
-            self.__size = size
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
+        """ initialize the private field size
+            set its value to 0 by default,
+            check the size field type and value before assign it """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
