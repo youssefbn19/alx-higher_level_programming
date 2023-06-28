8c3c2fc6dd54d86ed53d24901a0da46464ee073d
youssef <ybenchiir@gmail.com>
2023-06-28 08:45:50 -0700

    Add 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Contains empty class called Square """
+
+
+class Square:
+    """ Square class that defines a square """
+    def __init__(self, size=0):
+        """ initialize the private field size
+            set its value to 0 by default,
+            check the size field type and value before assign it """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Return the current square area"""
+        return self.__size ** 2
