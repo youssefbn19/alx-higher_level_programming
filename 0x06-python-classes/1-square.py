da9dec5c237e09088a386b2728f95c33c72ded4a
youssef <ybenchiir@gmail.com>
2023-06-28 08:12:21 -0700

    Add 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Contains empty class called Square """
+
+
+class Square:
+    """ Square class that defines a square """
+    def __init__(self, size):
+        self.__size = size
