78f6529b5cf8fb4b7f525780a91a53e45541dde3
youssef <ybenchiir@gmail.com>
2023-07-11 08:42:43 -0700

    Update

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -9,6 +9,9 @@ class MyList(list):
     """
     Simple class inherits from list
     """
+    def __init__(self):
+        super().__init__(self)
+
     def print_sorted(self):
         """
         prints the list, but sorted (ascending sort)
