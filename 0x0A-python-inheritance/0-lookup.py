a7b71dae95db10049feef0a768ad5ef7af973438
youssef <ybenchiir@gmail.com>
2023-07-11 08:03:40 -0700

    First Commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+The Module contains only lookup() function
+check the function __doc__ for more details
+"""
+
+
+def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object.
+    """
+    return dir(obj)
