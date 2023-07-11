9f1a0142069054a0a6ac49a6772f1e7739df9050
youssef <ybenchiir@gmail.com>
2023-07-11 11:39:27 -0700

    Add 7-base_geometry.py and its test file

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+The Module contains only BaseGeometry class
+check the class __doc__ for more details
+"""
+
+
+class BaseGeometry:
+    """
+    Simple class with area and integer_validator funtions
+    """
+    def area(self):
+        """
+        The area function only raises an Exception with a message
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        validates value argument
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
