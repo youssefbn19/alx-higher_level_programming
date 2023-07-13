42908bb852191816ba061758ec362414ce6fda7a
youssef <ybenchiir@gmail.com>
2023-07-12 19:58:06 -0700

    Add 11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+The Module contains class Student
+"""
+
+
+class Student:
+    """
+    class defines a student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of
+        a Student instance
+        """
+        if attrs is not None and all(type(at) is str for at in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance
+        """
+        self.__dict__ = json
