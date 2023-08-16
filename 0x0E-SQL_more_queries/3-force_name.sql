bf2f8a486b6d09eb428f254f60e936e5ca82d8ca
Youssef <ybenchiir@gmail.com>
2023-08-16 13:35:47 -0700

    first commit

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,5 @@
+-- Creates the table force_name id and name attributes.
+CREATE TABLE IF NOT EXISTS force_name(
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- Lists all the cities of California that can be found in the database hbtn_0d_usa.
+SELECT c.id, c.name, s.name FROM cities c
+INNER JOIN states s
+ON c.state_id = s.id
+ORDER BY c.id;
