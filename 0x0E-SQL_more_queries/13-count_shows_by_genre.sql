5e34ba0eba672cd0358bec7144d0fce74a34fedd
Youssef <ybenchiir@gmail.com>
2023-08-17 05:11:53 -0700

    Update query

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,4 +3,5 @@ SELECT g.name, COUNT(sg.genre_id) AS number_of_shows FROM tv_genres g
 JOIN tv_show_genres sg
 ON g.id = sg.genre_id
 GROUP BY sg.genre_id
+HAVING COUNT(sg.genre_id) > 0
 ORDER BY number_of_shows DESC;
