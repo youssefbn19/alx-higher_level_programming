0e96640e532044f07a5d6b73665e2e627b49a032
Youssef Benchiir <ybenchiir@gmail.com>
2023-09-11 22:57:33 +0100

    Add task 12

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// Searches the second biggest integer in the list of arguments.
+const args = process.argv;
+const len = args.length;
+const arr = [];
+
+if (len <= 3) {
+  console.log('0');
+} else {
+  for (let i = 2; i < len; i++) {
+    arr.push(Number(args[i]));
+  }
+  arr.sort();
+  console.log(arr[arr.length - 2]);
+}
