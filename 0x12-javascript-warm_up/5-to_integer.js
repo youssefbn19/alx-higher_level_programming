8addb7ceda8a043948d844e4f6dbf1cbfc2533c1
Youssef Benchiir <ybenchiir@gmail.com>
2023-09-11 22:01:38 +0100

    Update comment

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// Prints two arguments passed to it, in the following format: “ is ”.
+// Prints My number: <first argument converted in integer> if the first argument can be converted to an integer.
 const args = process.argv;
 const num = Number(args[2]);
 
