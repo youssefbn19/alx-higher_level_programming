0690aaf63cb2576bc595463ae630e7f6b068f8dd
Youssef Benchiir <ybenchiir@gmail.com>
2023-09-13 16:07:20 +0100

    Update

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,10 +3,10 @@
 const dict = require('./101-data').dict;
 const newDict = {};
 for (const key in dict) {
-  newDict[dict[key]] = [];
+  if (dict[key] in newDict) {
+    newDict[dict[key]].push(key);
+  } else {
+    newDict[dict[key]] = [key];
+  }
 }
-for (const key in dict) {
-  newDict[dict[key]].push(key);
-}
-
 console.log(newDict);
