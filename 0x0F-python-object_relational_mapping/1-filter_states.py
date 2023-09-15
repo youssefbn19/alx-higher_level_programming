a2fc148650464dcc2c974edf97ad947ec428a2d7
Youssef Benchiir <ybenchiir@gmail.com>
2023-09-15 11:48:17 +0100

    Update sql query

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,10 +8,16 @@ import sys
 
 
 def filter_states(params):
+    """
+    Filter states table with a name starting with N (upper N).
+    Args:
+        params (list): list  take 3 arguments: mysql username,
+        mysql password and database name 
+    """
     state_db = MySQLdb.connect(host="localhost", port=3306,
                                user=params[1], passwd=params[2], db=params[3])
     cr = state_db.cursor()
-    cr.execute("SELECT * FROM states WHERE name LIKE 'N%'\
+    cr.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'\
                ORDER BY states.id ASC")
     states = cr.fetchall()
     for state in states:
