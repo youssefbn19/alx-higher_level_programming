b12f2c36f128fb0cef2e0f3e612c3c79c5e99486
Youssef Benchiir <ybenchiir@gmail.com>
2023-09-15 11:49:28 +0100

    Fix pycodestyle error

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,7 @@ def filter_states(params):
     Filter states table with a name starting with N (upper N).
     Args:
         params (list): list  take 3 arguments: mysql username,
-        mysql password and database name 
+        mysql password and database name
     """
     state_db = MySQLdb.connect(host="localhost", port=3306,
                                user=params[1], passwd=params[2], db=params[3])
