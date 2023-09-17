3cd36c0a6968f239f834c7460a88276e38aadab3
Youssef Benchiir <ybenchiir@gmail.com>
2023-09-17 22:53:15 +0100

    Add

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+    The module contains the class definition of
+    a State and an instance Base = declarative_base().
+"""
+from sqlalchemy import (VARCHAR, Column,
+                        Integer, PrimaryKeyConstraint,
+                        UniqueConstraint)
+from sqlalchemy.orm import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    The class definition of a State table and an instance
+    Base = declarative_base()
+
+    Args:
+        Base (class): The declarative base class wraps the mapper
+        and the MetaData.
+    """
+    __tablename__ = "state"
+    id = Column(Integer, autoincrement=True)
+    name = Column(VARCHAR(128), nullable=False)
+    __table_args__ = (
+        PrimaryKeyConstraint("id", name="state_pk"),
+        UniqueConstraint("id", name="unique_id")
+    )
