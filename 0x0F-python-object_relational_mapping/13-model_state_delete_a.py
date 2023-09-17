0e7a5190e58b574758b8431820167b460b10c9e6
Youssef Benchiir <ybenchiir@gmail.com>
2023-09-18 00:37:28 +0100

    Add task 13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Change the name of a State object from the database hbtn_0e_6_usa
+    where id = 2 to New Mexico.
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from model_state import State
+
+if __name__ == "__main__":
+    engine = create_engine(
+             f'mysql://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}'
+             )
+    Session = sessionmaker(engine)
+    session = Session()
+    states = session.query(State).filter(State.name.contains("a")).delete()
+    session.commit()
