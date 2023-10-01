41ea2b849ccba64d570d553bb1418eca3d44bdfc
Youssef Benchiir <ybenchiir@gmail.com>
2023-10-01 15:39:33 +0100

    Update

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -sX GET -o /dev/null -w "You got me!" "0.0.0.0:5000/catch_me"
+curl -sLX PUT -H "Origin: School" -d "user_id=98" 0.0.0.0:5000/catch_me_2
