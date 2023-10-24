8e9c3ec267864ad25a6b8a50d7a36d5b35e7f943
Youssef Benchiir <ybenchiir@gmail.com>
2023-10-24 20:59:58 +0100

    Add new js file

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+// Display the status code of a GET request.
 const request = require('request');
 const args = process.argv.slice(2);
 request.get(args[0], function (error, response) {
