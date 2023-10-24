14be93f2833472c62337c4ea98b28b1dcc3680da
Youssef Benchiir <ybenchiir@gmail.com>
2023-10-24 19:55:47 +0100

    Add if statment

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,18 +1,20 @@
 #!/usr/bin/node
-// Prints the title of a Star Wars movie where the episode number matches a given integer.
+// Prints the number of movies where the character “Wedge Antilles” is present.
 const request = require('request');
 const args = process.argv.slice(2);
 const character = 'https://swapi-api.alx-tools.com/api/people/18/';
 let numMovies = 0;
-request.get(args[0], function (err, res, body) {
-  if (err) { return console.log(err); }
-  if (res.statusCode === 200) {
-    const data = JSON.parse(body);
-    for (const char of data.results) {
-      if (char.characters.includes(character)) {
-        numMovies++;
+if (args.length > 0) {
+  request.get(args[0], function (err, res, body) {
+    if (err) { return console.log(err); }
+    if (res.statusCode === 200) {
+      const data = JSON.parse(body);
+      for (const char of data.results) {
+        if (char.characters.includes(character)) {
+          numMovies++;
+        }
       }
+      console.log(numMovies);
     }
-    console.log(numMovies);
-  }
-});
+  });
+}
