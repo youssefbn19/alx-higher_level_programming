955793cbaead2922ed020469aca07232b8ab0433
Youssef Benchiir <ybenchiir@gmail.com>
2023-12-26 15:49:24 +0100

    Delete 0x15-javascript-web_jquery directory

--- 0x15-javascript-web_jquery/8-script.js
+++
@@ -1,7 +0,0 @@
-$.get("https://swapi-api.alx-tools.com/api/films/?format=json",
-    function (data) {
-        $.each(data.results, function (index, value) { 
-            $('UL#list_movies').append(`<li>${value.title}</li>`);
-        });
-    }
-);
--- 0x15-javascript-web_jquery/9-script.js
+++
@@ -1,5 +0,0 @@
-$.get("https://hellosalut.stefanbohacek.dev/?lang=fr",
-    function (data) {
-        $('DIV#hello').text(data.hello);
-    }
-);
